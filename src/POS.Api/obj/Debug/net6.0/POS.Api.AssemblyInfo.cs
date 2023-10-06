313630e2144d6f029bb70ffa3a5d76d4e3c783c2
roberto carlos soliz <robertoucb@hotmail.com>
2023-10-06 08:47:48 -0400

    configuration and deploy in docker Pos.Api

--- src/POS.Api/obj/Debug/net6.0/POS.Api.AssemblyInfo.cs
+++ src/POS.Api/obj/Debug/net6.0/POS.Api.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("157fa419-8e61-43b1-9680-2e297f75fd84")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
